92a511e6325fc434b970e77f289b6f0d03a57045
boussau <boussau@gmail.com>
2019-03-08 09:25:34 +0100

    Corrected the files for Archaea.

--- Archaea/commands.sh
+++ Archaea/commands.sh
@@ -5,15 +5,17 @@ python ../../Scripts/removeQuotesFromTree.py arc122_r86.1.tree arc122_r86.1.tree
 
 
 # Selection at level 1:
-python ../../Scripts/select_leaves.py arc122_r86.1.treeRN -d arc_metadata_r86.tsv -l 1 > outputSelectionLevel1
+python ../../Scripts/select_leaves_Archaea.py arc122_r86.1.treeRN -d arc_metadata_r86.tsv -l 1 > outputSelectionLevel1
 # Selection at level 2:
-python ../../Scripts/select_leaves.py arc122_r86.1.treeRN -d arc_metadata_r86.tsv -l 2 > outputSelectionLevel2
+python ../../Scripts/select_leaves_Archaea.py arc122_r86.1.treeRN -d arc_metadata_r86.tsv -l 2 > outputSelectionLevel2
 # Selection at level 3:
-python ../../Scripts/select_leaves.py arc122_r86.1.treeRN -d arc_metadata_r86.tsv -l 3 > outputSelectionLevel3
-
+python ../../Scripts/select_leaves_Archaea.py arc122_r86.1.treeRN -d arc_metadata_r86.tsv -l 3 > outputSelectionLevel3
+# Selection at level 3:
+python ../../Scripts/select_leaves_Archaea.py arc122_r86.1.treeRN -d arc_metadata_r86.tsv -l 4 > outputSelectionLevel4
 
 # extract a subtree with sampled genomes
 python ~/Documents/Utils/scripts/pruneTreeWithListOfLeaves.py arc122_r86.1.treeRN representant_1 sampled_level1.dnd
 python ~/Documents/Utils/scripts/pruneTreeWithListOfLeaves.py arc122_r86.1.treeRN representant_2 sampled_level2.dnd
 python ~/Documents/Utils/scripts/pruneTreeWithListOfLeaves.py arc122_r86.1.treeRN representant_3 sampled_level3.dnd
+python ~/Documents/Utils/scripts/pruneTreeWithListOfLeaves.py arc122_r86.1.treeRN representant_4 sampled_level4.dnd
 
