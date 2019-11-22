e39a483cf241c96c8716475d9d5ebaa1b06ec8fc
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2019-11-22 14:17:00 +0100

    Improved the renaming script a bit.

--- updated_concatenates/commands.sh
+++ updated_concatenates/commands.sh
@@ -1,6 +1,6 @@
 # Renaming, 2 steps:
 
-python Scripts/NEW_RenameLeavesAccordingToRefFile.py updated_concatenates/all_domains_concat.fas.treefileRN updated_concatenates/all_domains_concat.fas.treefile_renamed Archaea/Data/arc_metadata_r86.tsv
+python ../Scripts/NEW_RenameLeavesAccordingToRefFile.py all_domains_concat.fas.treefile all_domains_concat.fas.treefileRN ../Bacteria/Data/bac_metadata_r86.tsv
 
-python Scripts/NEW_RenameLeavesAccordingToRefFile.py updated_concatenates/all_domains_concat.fas.treefileRN updated_concatenates/all_domains_concat.fas.treefile_renamed Archaea/Data/arc_metadata_r86.tsv
+python ../Scripts/NEW_RenameLeavesAccordingToRefFile.py all_domains_concat.fas.treefileRN all_domains_concat.fas.treefile_renamed ../Archaea/Data/arc_metadata_r86.tsv
 
