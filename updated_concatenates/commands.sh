dd74f8f78ed831edc3a66a665f97563bb2f3c6ce
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2019-11-22 14:12:14 +0100

    Adding renamed tree file all_domains_concat.fas.treefile_renamed and command used to rename it.

---
+++ updated_concatenates/commands.sh
@@ -0,0 +1,6 @@
+# Renaming, 2 steps:
+
+python Scripts/NEW_RenameLeavesAccordingToRefFile.py updated_concatenates/all_domains_concat.fas.treefileRN updated_concatenates/all_domains_concat.fas.treefile_renamed Archaea/Data/arc_metadata_r86.tsv
+
+python Scripts/NEW_RenameLeavesAccordingToRefFile.py updated_concatenates/all_domains_concat.fas.treefileRN updated_concatenates/all_domains_concat.fas.treefile_renamed Archaea/Data/arc_metadata_r86.tsv
+
